f95da47485e3263df0cdd7798d3a81c190fbe4a7
SummerMedusa <50718811+SummerDarcel@users.noreply.github.com>
2019-10-01 18:25:56 -0700

    updated seeds

--- models/seeds.sql
+++ models/seeds.sql
@@ -2,7 +2,7 @@ USE animals;
 
 INSERT INTO pets (petName,petEnergy,petExercise,petTraining,petGrooming,petAffection,petDescription,petPhoto,petLink)
 VALUES
-("Fish",2, 1, 1, 1, 1, "Stay away from Pirannas","public/images/doryFish.jpeg");
+("Fish",2, 1, 1, 1, 1, "Stay away from Piranhas","public/images/doryFish.jpeg", "https://www.imdb.com/title/tt0464154/");
 
 INSERT INTO pets (petName,petEnergy,petExercise,petTraining,petGrooming,petAffection,petDescription,petPhoto,petLink)
 VALUES
@@ -10,32 +10,32 @@ VALUES
 
 INSERT INTO pets (petName,petEnergy,petExercise,petTraining,petGrooming,petAffection,petDescription,petPhoto,petLink)
 VALUES
-("Dog",4, 4, 5, 3, 5,"Can smell your feelings","public/images/dog.jpeg");
+("Dog",4, 4, 5, 3, 5,"Can smell your feelings","public/images/dog.jpeg", "https://www.petfinder.com/dog/petunia-46089976/ca/los-angeles/the-mutt-scouts-ca434/");
 
 INSERT INTO pets (petName,petEnergy,petExercise,petTraining,petGrooming,petAffection,petDescription,petPhoto,petLink)
 VALUES
-("Bird",3, 2, 3, 1, 1,"Loud and Messy","public/images/bird.jpeg");
+("Bird",3, 2, 3, 1, 1,"Loud and Messy","public/images/bird.jpeg","https://www.petfinder.com/bird/young-quaker-46148243/mi/battle-creek/birds-and-beaks-rescue-and-rehab-mi1023/");
 
 INSERT INTO pets (petName,petEnergy,petExercise,petTraining,petGrooming,petAffection,petDescription,petPhoto,petLink)
 VALUES
-("Snake",2, 2, 2, 1, 1,"Don't have evelids, so they are always watching","public/images/snake.jpg");
+("Snake",2, 2, 2, 1, 1,"Don't have eyelids, so they are always watching","public/images/snake.jpg", "https://www.petfinder.com/scales-fins-others/benedict-46144802/wa/bellingham/whatcom-humane-society-wa7/");
 
 INSERT INTO pets (petName,petEnergy,petExercise,petTraining,petGrooming,petAffection,petDescription,petPhoto,petLink)
 VALUES
-("Monkey",5, 5, 5, 5, 5,"Permenant Toddlers","public/images/monkey.jpg");
+("Monkey",5, 5, 5, 5, 5,"Permenant Toddlers","public/images/monkey.jpg", "https://en.wikipedia.org/wiki/Monkey");
 
 INSERT INTO pets (petName,petEnergy,petExercise,petTraining,petGrooming,petAffection,petDescription,petPhoto,petLink)
 VALUES
-("Pony",4, 5, 5, 5, 3,"Small horses","public/images/pony.jpg");
+("Pony",4, 5, 5, 5, 3,"It's a small horse","public/images/pony.jpg", "http://www.horsesensewirral.com/adopt-a-pony/");
 
 INSERT INTO pets (petName,petEnergy,petExercise,petTraining,petGrooming,petAffection,petDescription,petPhoto,petLink)
 VALUES
-("Hampster",3, 2, 1, 1, 1,"Can be moody","public/images/hamster-alone.jpg");
+("Hampster",3, 2, 1, 1, 1,"Can be moody","public/images/hamster-alone.jpg", "https://www.petfinder.com/small-furry/dwarf-hamsters-25562212/ny/brooklyn/sean-casey-animal-rescue-ny637/");
 
 INSERT INTO pets (petName,petEnergy,petExercise,petTraining,petGrooming,petAffection,petDescription,petPhoto,petLink)
 VALUES
-("Rock",1, 1, 1, 1, 1,"Does not fetch","public/images/rock.jpg");
+("Rock",1, 1, 1, 1, 1,"Does not fetch","public/images/rock.jpg", "https://twitter.com/TheRock");
 
 INSERT INTO pets (petName,petEnergy,petExercise,petTraining,petGrooming,petAffection,petDescription,petPhoto,petLink)
 VALUES
-("Unicorn",5, 5, 1, 5, 5,"Unicorn horns were used in pharmacies in the 1700's.","public/images/unicorn.jpg");
+("Unicorn",5, 5, 1, 5, 5,"Horns were used in pharmacies in the 1700's.","public/images/unicorn.jpg", "https://www.unicornadoptions.com/");
