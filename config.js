2ca4c8342095b75749d51b2d561707522c779fdc
Val <50930320+water96763@users.noreply.github.com>
2019-10-03 20:37:56 -0700

    cleaning files, deleting unnecessary files

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 var config = {
-    MY_KEY : '2f95f51b181ddd27883e91878e922466',
-    SECRET_KEY : 'lzE94uJm1bzOPJdw8xsLFAaHsQXBQo26zdWJ48iT'
-   
+  MY_KEY: "2f95f51b181ddd27883e91878e922466",
+  SECRET_KEY: "lzE94uJm1bzOPJdw8xsLFAaHsQXBQo26zdWJ48iT"
 };
